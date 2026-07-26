42ccc73e7a167f91f784677cd3ee13968d6c5ac9
Poveragente <154063416+Poveragente@users.noreply.github.com>
2026-07-26 15:06:12 +0300

    task_1 изменения

--- 03_lesson/lesson_3_task_1.py
+++ 03_lesson/lesson_3_task_1.py
@@ -2,3 +2,7 @@ import user
 
 my_user = user.User("Kirill", "DuboFF")
 
+print(my_user.first_name)
+print(my_user.last_name)
+print(my_user.first_name + " " + my_user.last_name)
+
--- 03_lesson/user.py
+++ 03_lesson/user.py
@@ -3,7 +3,4 @@ class User:
         self.first_name = first_name
         self.last_name = last_name
 
-        print(first_name)
-        print(last_name)
-        print(first_name + " " + last_name)
 
